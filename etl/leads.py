e0777c2b48293def26c94a95f5a5b68314a5351a
Wardormeur <wardormeur@gmail.com>
2018-08-21 11:16:11 +0100

    Remove end because python is not Pascal

--- etl/leads.py
+++ etl/leads.py
@@ -2,19 +2,14 @@ def transform_lead(row):  # Transform / Load for Lead Dimension
     completion = 0
     if (row['champion_is_valid'] == 'true'):
         completion += 1
-    end
     if (row['dojo_is_valid'] == 'true'):
         completion += 10
-    end
     if (row['venue_is_valid'] == 'true'):
         completion += 100
-    end
     if (row['team_is_valid'] == 'true'):
         completion += 1000
-    end
     if (row['charter_is_valid'] == 'true'):
         completion += 10000
-    end
     return (
         row['id'],
         row['user_id'],
